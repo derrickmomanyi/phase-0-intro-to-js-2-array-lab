bd35a3c8c2d4b896c9ba3cf9e5def5a5bbbf913a
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2022-11-17 10:14:39 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-// Write your solution here!
+let cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+    ];
+
+ console.log(cats)   
+
+function beforeEach (){
+    console.log("Run")
+     }
+
+
+     function destructivelyAppendCat(){
+    
+        cats.push("Ralph")
+        return cats;
+    }
+    
+   
+     function destructivelyPrependCat(){
+    
+        cats.unshift("Bob")
+        return cats;
+    }
+    
+    function destructivelyRemoveLastCat(){
+        cats.pop();
+        return cats;
+
+    }
+
+    function destructivelyRemoveFirstCat() {
+
+        cats.shift();
+        return cats;
+
+    }
+
+    function appendCat(name){
+
+        var newArray = cats.slice();
+        newArray.push("Broom")
+        return newArray
+      
+    }
+
+    function prependCat(name){
+
+        var newArray2 = cats.slice();
+        newArray2.unshift("Arnold")
+        return newArray2
+      
+    }
+
+    function removeLastCat(){
+
+        var newArray3 = cats.slice();
+        newArray3.pop();
+        return newArray3
+
+    }
+
+    function removeFirstCat(){
+
+        var newArray4 = cats.slice();
+        newArray4.shift();
+        return newArray4
+    }
